4a3455cae5e918429721eeeb650926ddd490a55e
Mateus Fiori <mateus.fiori@gruponewway.com.br>
2019-10-06 21:51:25 -0300

    Forecast response pattern implemented

--- service/ProphetService.py
+++ service/ProphetService.py
@@ -2,15 +2,23 @@ import pandas as pd
 from fbprophet import Prophet
 from datetime import datetime
 
-def generateJsonResult():
-    pass
+def generateJsonResult(forecastDf):
+    result = {'result': list()}
+    for _, row in forecastDf.iterrows():
+        result['result'].append({
+            'timestamp': str(row['ds']),
+            'yhat': row['yhat'],
+            'yhat_lower': row['yhat_lower'],
+            'yhat_upper': row['yhat_upper'],
+        })
+    return result
 
 def formatDictToDataframe(timeserie):
     return {
-        'ds': [str(datetime.fromtimestamp(int(ts))) for ts in list(timeserie.keys())],
+        'ds': [str(datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d %H:%M:%S')) for ts in list(timeserie.keys())],
         'y': list(timeserie.values())
     }
 
 def forecastTimeserie(timeserie):
     model = Prophet().fit(pd.DataFrame.from_dict(formatDictToDataframe(timeserie)))
-    return model.predict(model.make_future_dataframe(periods=10))
+    return generateJsonResult(model.predict(model.make_future_dataframe(periods=30, freq='S')))
